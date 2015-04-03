5ac0df2de8a6fd5173e6dca06917969379c9748f
Henry J Schmale <hschmale16@gmail.com>
2015-04-03 19:59:09 -0400

    The Setup Script is much improved
    It finds files, and generates SQLite querries correctly.

--- setup.sh
+++ setup.sh
@@ -9,21 +9,60 @@
 # * ffprobe
 # * sqlite3
 
-
+# Script Setup
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 
 # database file
 dbf=ConsoleMP.db 
-# tmp file for storing tmp data
+# tmp file for storing tmp song data
 mytmp=/tmp/songdata.junk
+# tmp file for storing sql statements
+sqlTMPFile=/tmp/sqlTMP.junk
+
+# ***********************************************
+# Functions
+# ***********************************************
+# \param $1 - The field to print contents of
+function printField {
+    echo $(cat $mytmp | grep $1 | head -1 | cut -d: -f2 | sed "s/'/''/g")
+
+}
+
+# \param $1 string to escape
+# \return the escaped string
+function escapeStrForSQLite {
+    echo $1 | sed "s/'/''/g"
+}
+
+# ***********************************************
+# Begin Main Script
+# ***********************************************
 
 # Create Database Table
-
+sqlite3 $dbf \
+    'CREATE TABLE if not exists `songs` (
+        `id`        INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+        `title`     TEXT NOT NULL,
+        `artist`    TEXT NOT NULL,
+        `album`     TEXT,
+        `path`      TEXT NOT NULL UNIQUE,
+        `year`      INTEGER,
+        `rating`    INTEGER DEFAULT 0,
+        `played`    INTEGER DEFAULT 0
+     );'
 
 # Iterate over files in home folder to generate list of songs
 for f in $(find ~/Music -name "*.mp3") ; do
-    path=$f                            # File Path
+    path=$(escapeStrForSQLite $f ) # File Path
     ffprobe $path 2>&1 | tail -20 > $mytmp
-        
+    title=$(printField title)
+    artist=$(printField artist)
+    year=$(printField date)
+    album=$(printField album)
+    sql="Insert into songs(title, artist, year, album, path) values(
+    '$title', '$artist', '$year', '$album', '$path');"
+    echo $sql >> $sqlTMPFile
 done
+
+
