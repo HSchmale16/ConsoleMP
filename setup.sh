fe1ad17fee169511e65a9ef9e46390c92fe75b57
Henry J Schmale <hschmale16@gmail.com>
2015-04-03 18:58:15 -0400

    Script Stuff

--- setup.sh
+++ setup.sh
@@ -3,23 +3,27 @@
 # Builds the database, and finds songs
 # Henry J Schmale
 # March 18, 2015
+# 
+# ***********************************************
+# Depends:
+# * ffprobe
+# * sqlite3
+
 
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 
 # database file
 dbf=ConsoleMP.db 
+# tmp file for storing tmp data
+mytmp=/tmp/songdata.junk
 
 # Create Database Table
-sqlite3 $dbf "Create Table if not exists 
-                     SONGS(ID INTEGER PRIMARY KEY,
-                           NAME TEXT,
-                           AUTHOR TEXT,
-                           PATH TEXT,
-                           RATING INTEGER,
-                           PLAYCOUNT INTEGER);"
+
 
 # Iterate over files in home folder to generate list of songs
 for f in $(find ~/Music -name "*.mp3") ; do
-    echo $f
+    path=$f                            # File Path
+    ffprobe $path 2>&1 | tail -20 > $mytmp
+        
 done
